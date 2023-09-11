7dc2dbabbe0af7812a6628ed1d06a23fc8c4a269
MEN1213 <g212033@st.t-kougei.ac.jp>
2023-09-11 12:43:33 +0900

    ゴール判定、リザルトのテスト

---
+++ Assets/script/Gaoal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gaoal : MonoBehaviour
+{
+    // public static bool goal;
+    // Start is called before the first frame update
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="Player" )
+        {
+            Debug.Log("ÉSÅ[Éã");
+        }
+    }
+}
